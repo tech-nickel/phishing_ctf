f00e503ea180578e2c828967b9f72042ffd06e11
Richard Riss <richard.riss@desy.de>
2022-11-30 11:32:41 +0100

    Quellcode text matches email text

--- src/js/custom_script.js
+++ src/js/custom_script.js
@@ -3,3 +3,13 @@
     var retval = arr.join(" ") + "...";
     return retval;
   };
+
+  window.bodies_html = function (val) {
+    var arr = val.split(/\r?\n|\r|\n/g);
+    var arr_new = [];
+    for (var i = 0; i < arr.length; i++) {
+      arr_new.push("&lt;p&gt;" + arr[i] + "&lt;/p&gt;")
+    }
+    var retval = arr_new.join("\r\n");
+    return retval;
+  };
